01b8103a4f0152d9b8d8d9f3d0065aad7371f95b
Pr0ny <mimoone.ml@gmail.com>
2016-10-15 15:46:23 +0200

    NIQYUE TA RACE GITHUB AVEC TES MERGES DE MES COUILLES A DEUX BALLES

---
+++ test/client/client.cpp
@@ -0,0 +1,21 @@
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[])
+{
+}
